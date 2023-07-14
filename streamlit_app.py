b71cdeb5c8a96906020709f848ac76311240fa3f
dballoun <138248946+dballoun@users.noreply.github.com>
2023-07-14 14:43:59 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -42,3 +42,8 @@ my_cur.execute("select * from pc_rivery_db.public.fruit_load_list")
 my_data_rows = my_cur.fetchall()
 streamlit.header("The fruit load list contains:")
 streamlit.dataframe(my_data_rows)
+
+# Allow the end user to add a fruit to the list
+add_my_fruit = 'jackfruit'
+streamlit.text("What fruit would you like to add?")
+streamlit.dataframe(add_my_fruit)
